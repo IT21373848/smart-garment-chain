5a3fe98d49b0d9a671895d27002a3f0509503862
Nimsara Mahagedara <dknimzara@gmail.com>
2024-12-04 01:43:15 +0530

    fix-ui-estimation

--- apps/frontend/src/components/PredictTime.tsx
+++ apps/frontend/src/components/PredictTime.tsx
@@ -49,37 +49,44 @@ function PredictTime({ }: Props) {
     return (
         <div className='w-full min-w-96'>
             <h1>Predict Time</h1>
+            <div className='flex flex-col gap-3 p-4 rounded-xl bg-gray-500/50'>
+                <div className='flex flex-col bg-[#F3D49B] rounded-xl p-4 items-center justify-center gap-3 text-black w-full'>
+                    {error && <p className='text-red-500'>{error}</p>}
+                    <label className='text-white' htmlFor="item">Item</label>
+                    <select className='text-black' name="item" id="item" value={item} onChange={(e) => setItem(e.target.value)}>
+                        {uniqueClothingItems.map((item) => (
+                            <option key={item} value={item}>{item}</option>
+                        ))}
+                    </select>
 
-            <div className='flex flex-col bg-white/10 rounded-xl items-center justify-center gap-3 text-black w-full'>
-                {error && <p className='text-red-500'>{error}</p>}
-                <label className='text-white' htmlFor="item">Item</label>
-                <select className='text-black' name="item" id="item" value={item} onChange={(e) => setItem(e.target.value)}>
-                    {uniqueClothingItems.map((item) => (
-                        <option key={item} value={item}>{item}</option>
-                    ))}
-                </select>
+                    <label className='text-white' htmlFor="qty">Qty</label>
+                    <input type="number" name="qty" id="qty" value={qty} onChange={(e) => setQty(parseInt(e.target.value))} />
 
-                <label className='text-white' htmlFor="qty">Qty</label>
-                <input type="number" name="qty" id="qty" value={qty} onChange={(e) => setQty(parseInt(e.target.value))} />
+                    <label className='text-white' htmlFor="lines">Lines</label>
+                    <input type="number" name="lines" id="lines" value={lines} onChange={(e) => setLines(parseInt(e.target.value))} />
 
-                <label className='text-white' htmlFor="lines">Lines</label>
-                <input type="number" name="lines" id="lines" value={lines} onChange={(e) => setLines(parseInt(e.target.value))} />
+                    <label className='text-white' htmlFor="employees">Employees</label>
+                    <input type="number" name="employees" id="employees" value={employees} onChange={(e) => setEmployees(parseInt(e.target.value))} />
 
-                <label className='text-white' htmlFor="employees">Employees</label>
-                <input type="number" name="employees" id="employees" value={employees} onChange={(e) => setEmployees(parseInt(e.target.value))} />
+                    {/* elapsed time */}
+                    <div>
+                        <p className='text-white'>Elapsed Time: {time || 0}</p>
+                        <input type="number" value={time} onChange={(e) => setTime(parseInt(e.target.value))} />
+                    </div>
 
-                {/* elapsed time */}
-                <div>
-                    <p className='text-white'>Elapsed Time: {time || 0}</p>
-                    <input type="number" value={time} onChange={(e) => setTime(parseInt(e.target.value))} />
                 </div>
-
-                <button className='bg-green-800 hover:bg-green-900 text-white font-bold py-2 px-4 rounded' onClick={handleEstimation}>Predict</button>
+                <button className='bg-yellow-800 hover:bg-green-900 text-white font-bold py-2 px-4 rounded-full' onClick={handleEstimation}>Predict</button>
             </div>
 
-            <div className='bg-white rounded-xl p-3 border shadow-sm my-5 text-black'>
-                <p>Estimated Time (in hours):</p>
-                <p >{estimatedTime}</p>
+
+            <div className='bg-white rounded-xl p-3 border shadow-sm my-5 text-black flex items-center justify-between gap-4'>
+                <div>
+                    <p>Estimated Time (in hours):</p>
+                    <p >{estimatedTime}</p>
+                </div>
+                <div className='w-20 h-20'>
+                    <img src="https://media.istockphoto.com/id/920345886/vector/classic-wristwatch-icon.jpg?s=612x612&w=0&k=20&c=MbgyLy52hlVIOqppuIpML4XSuxFUJozffHVH0Q5jMZ4=" className='w-full h-full object-contain' />
+                </div>
             </div>
         </div>
     )
