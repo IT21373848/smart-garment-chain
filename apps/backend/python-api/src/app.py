e69aa18d2672714c59d908a210f635fcc2956680
YasithaRenuk <126389901+YasithaRenuk@users.noreply.github.com>
2025-03-14 21:05:23 +0530

    Update app.py

--- apps/backend/python-api/src/app.py
+++ apps/backend/python-api/src/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
-# from routes.index import index as predictRoutes  # Import the Blueprint object
-# from routes.packingController import packingBluePrint
+from routes.index import index as predictRoutes  # Import the Blueprint object
+from routes.packingController import packingBluePrint
 from routes.supplierSelection import packingBluePrint
 
 app = Flask(__name__)
@@ -9,8 +9,8 @@ CORS(app)
 
 
 # Register the Blueprint
-# app.register_blueprint(predictRoutes)
-# app.register_blueprint(packingBluePrint)
+app.register_blueprint(predictRoutes)
+app.register_blueprint(packingBluePrint)
 app.register_blueprint(packingBluePrint)
 
 if __name__ == '__main__':
