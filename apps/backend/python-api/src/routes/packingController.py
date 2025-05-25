f6a15c47be104dd21e4d17d1ad6f26933bb05752
Sauni Weerasinghe <it21373848@my.sliit.lk>
2025-05-25 12:06:18 +0530

    packing implementation

--- apps/backend/python-api/src/routes/packingController.py
+++ apps/backend/python-api/src/routes/packingController.py
@@ -1,14 +1,21 @@
 from flask import Blueprint, request, jsonify
+from services.packing.packingService import generate_packing_plan
 
 packingBluePrint = Blueprint('packingController', __name__) 
 
 @packingBluePrint.route('/packing-prediction', methods=['POST'])
 def packingController():
     try:
-        input_data = request.json.get('input_data', [])
-        if not input_data:
-            return jsonify({"error": "No input data provided"}), 400
-        
-        return jsonify({"input_data": input_data})
+        data = request.json
+        box_data = data.get("box_data")
+        container_type = data.get("container_type")
+
+        if not box_data or not container_type:
+            return jsonify({"error": "Missing required parameters"}), 400
+
+        result = generate_packing_plan(box_data, container_type)
+
+        return jsonify(result)
+
     except Exception as e:
         return jsonify({"error": str(e)}), 500
