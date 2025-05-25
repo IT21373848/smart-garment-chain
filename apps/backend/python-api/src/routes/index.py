a8fc71799cf705eaad18f739b884978d64596b7f
Nimsara Mahagedara <dknimzara@gmail.com>
2025-05-25 19:26:42 +0530

    fiX:open-ai-api-key

--- apps/backend/python-api/src/routes/index.py
+++ apps/backend/python-api/src/routes/index.py
@@ -4,6 +4,11 @@ from services.estimate import predict_man_hours  # Import the service function
 # Create a Blueprint instance
 index = Blueprint('index', __name__)
 
+# health api
+@index.route('/health', methods=['GET'])
+def health():
+    return jsonify({"status": "OK"})
+
 @index.route('/predict', methods=['POST'])
 def predict():
     # Call the service function to handle the prediction
