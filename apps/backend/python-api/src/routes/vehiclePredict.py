5199f11266607ad61823358f44be88b78f5fc296
Sauni Weerasinghe <it21373848@my.sliit.lk>
2025-05-25 12:09:58 +0530

    remove changes of vehicle predict

--- apps/backend/python-api/src/routes/vehiclePredict.py
+++ apps/backend/python-api/src/routes/vehiclePredict.py
@@ -1,25 +1,25 @@
-# from flask import Blueprint, request, jsonify
-# from utils.vehicle_utils import optimal_vehicle_strategy
-# from data.vehicles import vehicles
+from flask import Blueprint, request, jsonify
+from utils.vehicle_utils import optimal_vehicle_strategy
+from data.vehicles import vehicles
 
-# predictVehicle = Blueprint('predictVehicle', __name__)
+predictVehicle = Blueprint('predictVehicle', __name__)
 
-# @predictVehicle.route('/predict-vehicle', methods=['POST'])
-# def recommend_vehicle():
-#     data = request.json
-#     deliveries = data['deliveries']
-#     distance = data['distance_km']
+@predictVehicle.route('/predict-vehicle', methods=['POST'])
+def recommend_vehicle():
+    data = request.json
+    deliveries = data['deliveries']
+    distance = data['distance_km']
 
-#     total_volume = sum(d['volume_cft'] for d in deliveries)
-#     total_weight = sum(d['weight_kg'] for d in deliveries)
+    total_volume = sum(d['volume_cft'] for d in deliveries)
+    total_weight = sum(d['weight_kg'] for d in deliveries)
 
-#     optimal_strategy = optimal_vehicle_strategy(total_volume, total_weight, distance)
+    optimal_strategy = optimal_vehicle_strategy(total_volume, total_weight, distance)
 
-#     return jsonify({
-#         "strategy": optimal_strategy['strategy'],
-#         "vehicles": optimal_strategy['vehicles'],
-#         "total_estimated_cost_LKR": optimal_strategy['estimated_cost_LKR'],
-#         "total_distance_km": distance,
-#         "total_volume_cft": total_volume,
-#         "total_weight_kg": total_weight
-#     })
+    return jsonify({
+        "strategy": optimal_strategy['strategy'],
+        "vehicles": optimal_strategy['vehicles'],
+        "total_estimated_cost_LKR": optimal_strategy['estimated_cost_LKR'],
+        "total_distance_km": distance,
+        "total_volume_cft": total_volume,
+        "total_weight_kg": total_weight
+    })
